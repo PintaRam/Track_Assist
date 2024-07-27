f8600dba774d8d18e2da4b57d09af77122319336
Pintaram <pintu.borana20@gmail.com>
2024-07-27 11:04:42 +0530

    Splash Creation and Selection of user is completed

--- app/src/main/java/com/track_assist/Splash.java
+++ app/src/main/java/com/track_assist/Splash.java
@@ -1,24 +1,36 @@
  package com.track_assist;
 
-import android.os.Bundle;
+ import android.content.Intent;
+ import android.os.Bundle;
+ import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+ import androidx.activity.EdgeToEdge;
+ import androidx.appcompat.app.AppCompatActivity;
+ import androidx.core.graphics.Insets;
+ import androidx.core.view.ViewCompat;
+ import androidx.core.view.WindowInsetsCompat;
 
-public class Splash extends AppCompatActivity {
+ public class Splash extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         EdgeToEdge.enable(this);
+         setContentView(R.layout.activity_splash);
+         Handler handler = new Handler();
+         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+             return insets;
+         });
+         handler.postDelayed(new Runnable() {
+             @Override
+             public void run() {
+                 Intent intent = new Intent(Splash.this,selectionOFUser.class);
+                 startActivity(intent);
+                 finish();
+             }
+         }, 6000);
+
+     }
+ }
--- app/src/main/java/com/track_assist/selectionOFUser.java
+++ app/src/main/java/com/track_assist/selectionOFUser.java
@@ -1,6 +1,10 @@
 package com.track_assist;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class selectionOFUser extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,23 @@ public class selectionOFUser extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button pat ,guide;
+        pat = findViewById(R.id.pat);
+        guide = findViewById(R.id.guide);
+        pat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(selectionOFUser.this,patientsRegistration.class);
+                startActivity(i);
+            }
+        });
+        guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(selectionOFUser.this,patientsRegistration.class);
+                startActivity(i);
+            }
         });
     }
 }
