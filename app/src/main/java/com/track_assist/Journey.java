2e002f4980fad99b68a02d9b0e56b674ad7bc760
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-07-28 12:50:18 +0530

    journey activity Under Progress

---
+++ app/src/main/java/com/track_assist/Journey.java
@@ -0,0 +1,14 @@
+package com.track_assist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Journey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journey);
+    }
+}
